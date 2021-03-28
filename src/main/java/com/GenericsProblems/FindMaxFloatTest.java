92ad486eb6fac00b4d212cec65e23aeab40cf555
ashwani161 <ashwanin65@gmail.com>
2021-03-28 15:25:08 +0530

    Added:Given Max Float number at 2nd  position and return same number

--- src/main/java/com/GenericsProblems/FindMaxFloatTest.java
+++ src/main/java/com/GenericsProblems/FindMaxFloatTest.java
@@ -11,6 +11,13 @@ public class FindMaxFloatTest {
 	Assert.assertTrue("Verifying maximum value", i==FindMax.findMax(i, j, k));
 
 	}
+	
+	@Test
+	public void giventhreefloats_WhenShouldReturnMaxAt2ndPosition() {
+		float i = 3.2f, j = 4.5f , k = 1.5f;
+		FindMaxFloat FindMax = new FindMaxFloat();
+	Assert.assertTrue("Verifying maximum value", j==FindMax.findMax(i, j, k));
 
+	}
 	
 }
